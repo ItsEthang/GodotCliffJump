b6b3e904d216f1a4765a5c17fc76ba89b89729ac
Ethang <52384982+ItsEthang@users.noreply.github.com>
2022-10-27 12:54:15 -0400

    Add files via upload

---
+++ scripts/Player.gd
@@ -0,0 +1,78 @@
+extends RigidBody2D
+# variables for vertical and horizontal speed
+# exporting camera node path variable
+export (NodePath) var camera_path
+var camera
+# grab the world scene
+var world = 'res://scenes/World.tscn'
+
+#player speeds
+var jumpSpeed = 90
+var strafe = 0
+var strafeAccel = 25
+#avatar sprite
+var sprite
+# viewport dimensions
+var width
+var height
+# all functions get called on start.
+
+func _ready():
+	camera = get_node(camera_path)
+	width = get_viewport_rect().size.x
+	height = get_viewport_rect().size.y
+	# grab the player's sprite
+	sprite = get_node("Sprite")
+	pass # Replace with function body.
+
+# Physics Process: kind fo like window.requestAnimationFrame(delta)
+func _physics_process(delta):
+	# get action keys
+	var leftKey = Input.is_action_pressed("ui_left")
+	var rightKey = Input.is_action_pressed("ui_right")
+	if leftKey and !rightKey:
+		# set x velocity, keep y velocity
+		if strafe > 0:
+			strafe = 0
+		strafe -= strafeAccel
+		# flip the sprite left
+		sprite.set_flip_h(true)
+		set_linear_velocity(Vector2(strafe, get_linear_velocity().y))
+	if rightKey and !leftKey:
+		if strafe < 0:
+			strafe = 0
+		strafe += strafeAccel
+		# unflip the sprite(facing right)
+		sprite.set_flip_h(false)
+		set_linear_velocity(Vector2(strafe, get_linear_velocity().y))
+	#if !leftKey and !rightKey:
+	#	strafe = 0 
+	#	set_linear_velocity(Vector2(strafe, get_linear_velocity().y))
+	if (leftKey and rightKey) or (!leftKey and !rightKey):
+		if (strafe > 0) : 
+			strafe -= strafeAccel
+			set_linear_velocity(Vector2(strafe, get_linear_velocity().y))
+		if (strafe < 0) :
+			strafe += strafeAccel
+			set_linear_velocity(Vector2(strafe, get_linear_velocity().y))
+	pass
+
+
+func collision(body):
+	#if player collides with mushroom's area2D
+	if body.is_in_group('mushrooms'):
+		# reverse the vertical velocity
+		set_linear_velocity(Vector2(0, -(get_linear_velocity().y)-jumpSpeed))
+	pass # Replace with function body.
+
+
+func exit_screen():
+	# avatar move out of frame
+	if position.x > camera.position.x and get_linear_velocity().x > 0 :
+		position = Vector2(-width/2, position.y)
+	if position.x < camera.position.x and get_linear_velocity().x < 0:
+		position = Vector2(width/2, position.y)
+	#avatar falls out of frame, reset the world
+	if position.y > (camera.position.y + height/2 + 120):
+		get_tree().change_scene(world)
+	pass # Replace with function body.
---
+++ scripts/mvMushroom.gd
@@ -0,0 +1,50 @@
+extends RigidBody2D
+export (NodePath) var camera_path
+var camera
+var strafe = 0
+var cnstSpd = 30
+var random = RandomNumberGenerator.new()
+var width
+var timePassed = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	camera = get_node(camera_path)
+	width = get_viewport_rect().size.x
+	random.randomize()
+	strafe = random.randf_range(-150, 150)
+	if strafe < 0:
+		strafe -= cnstSpd
+	if strafe > 0:
+		strafe += cnstSpd
+	set_linear_velocity(Vector2(strafe, 0))
+	# set_linear_velocity(Vector2(0, 0))
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	#initialize random strafing velocity
+	timePassed += delta;
+
+	# randomize the strafing velocity again
+	if timePassed >= 0.75:
+		timePassed = 0
+		random.randomize()
+		strafe = random.randf_range(-200, 200)
+		if strafe < 0:
+			strafe -= cnstSpd
+		if strafe > 0:
+			strafe += cnstSpd
+		set_linear_velocity(Vector2(strafe, 0))
+		# set_linear_velocity(Vector2(0, 0))
+	# print(strafe)
+	pass
+	
+
+
+# so mushroom can teleport to the opposite side when going out of screen.
+func exit_screen():
+	if position.x > camera.position.x and get_linear_velocity().x > 0 :
+		position = Vector2(-width/2, position.y)
+	if position.x < camera.position.x and get_linear_velocity().x < 0:
+		position = Vector2(width/2, position.y)
+	pass # Replace with function body.
