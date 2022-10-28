3f3be53d1fb4cc79bc6ce1e2a5bc0b941ffa1605
hhelsper <89586701+hhelsper@users.noreply.github.com>
2022-10-28 19:31:25 -0400

    hayess work today

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,8 +3,36 @@ extends RigidBody2D
 # exporting camera node path variable
 export (NodePath) var camera_path
 export (NodePath) var mushroom_path
+export (NodePath) var mushroom2_path
+export (NodePath) var mushroom3_path
 var camera
+
+var mushroom1Scene = preload ("res://instances/Mushroom1.tscn")
+var mushroom2Scene = preload ("res://instances/Mushroom2.tscn")
+var mushroom3Scene = preload ("res://instances/Mushroom3.tscn")
+
+var mush1 = mushroom1Scene.instance()
+var mush2 = mushroom2Scene.instance()
+var mush3 = mushroom3Scene.instance()
+
 var mushroom
+var mushroom2
+var mushroom3
+
+#jump counts
+var mushroom_jump_count = 0
+var mushroom2_jump_count = 0
+var mushroom3_jump_count = 0
+
+#booleans for mushroom death
+var mush1_death = false
+var mush2_death = false
+var mush3_death = false
+
+# total collisions since death per mushroom 
+var num_collisions_since_mush_death = 0
+var num_collisions_since_mush2_death = 0
+var num_collisions_since_mush3_death = 0
 # grab the world scene
 var world = 'res://scenes/World.tscn'
 
@@ -22,6 +50,8 @@ var height
 func _ready():
 	camera = get_node(camera_path)
 	mushroom = get_node(mushroom_path)
+	mushroom2 = get_node(mushroom2_path)
+	mushroom3 = get_node(mushroom3_path)
 	width = get_viewport_rect().size.x
 	height = get_viewport_rect().size.y
 	# grab the player's sprite
@@ -62,12 +92,66 @@ func _physics_process(delta):
 
 
 func collision(body):
+	
+	if mush1_death:
+		num_collisions_since_mush_death += 1
+		if num_collisions_since_mush_death == 2:
+			num_collisions_since_mush_death = 0
+			mush1_death = false
+			add_child(mush1)
+	if mush2_death:
+		num_collisions_since_mush2_death += 1
+		if num_collisions_since_mush2_death == 2:
+			num_collisions_since_mush2_death = 0
+			mush2_death = false
+			add_child(mush2)
+	if mush3_death:
+		num_collisions_since_mush3_death += 1
+		if num_collisions_since_mush3_death == 2:
+			num_collisions_since_mush3_death = 0
+			mush3_death = false
+			add_child(mush3)
 	#if player collides with mushroom's area2D
 	if body.is_in_group('mushrooms'):
+		# increase mushroom jump count
+		mushroom_jump_count += 1
+		
 		# reverse the vertical velocity
 		set_linear_velocity(Vector2(0, -(get_linear_velocity().y)-jumpSpeed))
 		$AnimationPlayer.play("jump")
 		mushroom.play("Landed")
+		if mushroom_jump_count == 3:
+			mushroom.queue_free()
+			mushroom_jump_count = 0
+			mush1_death = true
+		
+			
+	if body.is_in_group('mushrooms2'):
+		#increase mushroom 2 jump count
+		mushroom2_jump_count += 1
+		
+		# reverse the vertical velocity
+		set_linear_velocity(Vector2(0, -(get_linear_velocity().y)-jumpSpeed))
+		$AnimationPlayer.play("jump")
+		mushroom2.play("Landed")
+		if mushroom2_jump_count == 3:
+			mushroom2.queue_free()
+			mushroom2_jump_count = 0
+			mush2_death = true
+			
+	if body.is_in_group('mushrooms3'):
+		#increase mushroom 3 jump count
+		mushroom3_jump_count += 1
+		
+		# reverse the vertical velocity
+		set_linear_velocity(Vector2(0, -(get_linear_velocity().y)-jumpSpeed))
+		$AnimationPlayer.play("jump")
+		mushroom3.play("Landed")
+		if mushroom3_jump_count == 3:
+			mushroom3.queue_free()
+			mushroom3_jump_count = 0
+			mush3_death = true
+			
 	pass # Replace with function body.
 
 
